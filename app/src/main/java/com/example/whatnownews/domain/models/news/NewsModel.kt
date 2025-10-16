1f2ebc662ab197fb005971e36b112abecaa135bd
M.Hamed <beatlessmine@gmail.com>
2025-10-16 16:39:23 +0300

    feat: Create NewsModel data class
    Creates the `NewsModel` data class in the domain layer, which contains a list of `ArticleModel`.

---
+++ app/src/main/java/com/example/whatnownews/domain/models/news/NewsModel.kt
@@ -0,0 +1,8 @@
+package com.example.whatnownews.domain.models.news
+
+import com.example.whatnownews.domain.models.articles.ArticleModel
+
+data class NewsModel(
+    val articleModel: ArrayList<ArticleModel>
+)
+
