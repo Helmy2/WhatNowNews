2c6339d01317e4668e0b35dfbeeda66962565112
M.Hamed <beatlessmine@gmail.com>
2025-10-16 16:40:28 +0300

    Refactor: Update AuthRepository import path
    The import path for `AuthRepository` in `SignOutUseCase` has been moved to a new `auth` subpackage.

--- app/src/main/java/com/example/whatnownews/domain/usecase/auth/SignOutUseCase.kt
+++ app/src/main/java/com/example/whatnownews/domain/usecase/auth/SignOutUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.whatnownews.domain.usecase.auth
 
-import com.example.whatnownews.domain.repository.AuthRepository
+import com.example.whatnownews.domain.repository.auth.AuthRepository
 
 class SignOutUseCase(private val repository: AuthRepository) {
     operator fun invoke() = repository.signOut()
