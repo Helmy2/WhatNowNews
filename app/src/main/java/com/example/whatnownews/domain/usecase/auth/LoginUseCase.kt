e36a04c3533f400e427f2326e96a482832d96bef
M.Hamed <beatlessmine@gmail.com>
2025-10-16 16:37:53 +0300

    Refactor: Update LoginUseCase import path
    Moves `AuthRepository` to a more specific `auth` subpackage.

--- app/src/main/java/com/example/whatnownews/domain/usecase/auth/LoginUseCase.kt
+++ app/src/main/java/com/example/whatnownews/domain/usecase/auth/LoginUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.whatnownews.domain.usecase.auth
 
-import com.example.whatnownews.domain.repository.AuthRepository
+import com.example.whatnownews.domain.repository.auth.AuthRepository
 
 class LoginUseCase(private val authRepository: AuthRepository) {
     suspend operator fun invoke(email: String, password: String) = authRepository.login(email, password)
