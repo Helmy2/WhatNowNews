93e3d73431ca2d346d12991ccdc59c2ad04a4caf
M.Hamed <beatlessmine@gmail.com>
2025-10-16 17:33:35 +0300

    Refactor: Rename articleModel to articles in NewsModel

--- app/src/main/java/com/example/whatnownews/domain/models/news/NewsModel.kt
+++ app/src/main/java/com/example/whatnownews/domain/models/news/NewsModel.kt
@@ -3,6 +3,6 @@ package com.example.whatnownews.domain.models.news
 import com.example.whatnownews.domain.models.articles.ArticleModel
 
 data class NewsModel(
-    val articleModel: ArrayList<ArticleModel>
+    val articles: ArrayList<ArticleModel>
 )
 
