0d7eac77a52f274821e43abb1c2f216a234552be
M.Hamed <beatlessmine@gmail.com>
2025-10-16 16:36:57 +0300

    Refactor: Remove FavoriteArticlesRepository interface

--- app/src/main/java/com/example/whatnownews/data/repository/articles/FavoriteArticlesRepositoryImpl.kt
+++ app/src/main/java/com/example/whatnownews/data/repository/articles/FavoriteArticlesRepositoryImpl.kt
@@ -1,12 +1,12 @@
-package com.example.whatnownews.data.repository
+package com.example.whatnownews.data.repository.articles
 
 import com.example.whatnownews.data.remote.favorites.FavoriteArticlesDS
-import com.example.whatnownews.domain.model.FavoriteArticlesModel
-import com.example.whatnownews.domain.repository.ArticleRepository
+import com.example.whatnownews.domain.models.articles.FavoriteArticlesModel
+import com.example.whatnownews.domain.repository.articles.FavoriteArticlesRepository
 
-class ArticleRepositoryImpl(
+class FavoriteArticlesRepositoryImpl(
     private val remote: FavoriteArticlesDS
-) : ArticleRepository {
+) : FavoriteArticlesRepository {
 
     override suspend fun getArticles(): List<FavoriteArticlesModel> {
         return remote.getArticles()
