cbe154d83400fa1a29670a22f73c078c74ddfd2b
M.Hamed <beatlessmine@gmail.com>
2025-10-16 16:40:49 +0300

    Refactor: Update SettingRepository import path in SplashViewModel

--- app/src/main/java/com/example/whatnownews/presentation/auth/SplashViewModel.kt
+++ app/src/main/java/com/example/whatnownews/presentation/auth/SplashViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.whatnownews.presentation.auth
 
 import androidx.lifecycle.ViewModel
-import com.example.whatnownews.domain.repository.SettingRepository
+import com.example.whatnownews.domain.repository.settings.SettingRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
