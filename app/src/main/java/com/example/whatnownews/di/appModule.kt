41047d245acdf65f9b0eca482bcfd3794d8d9eb8
helmy <mohelmy.eng@gmail.com>
2025-10-16 07:56:08 +0300

    Implement logout functionality and refactor DI

--- app/src/main/java/com/example/whatnownews/di/appModule.kt
+++ app/src/main/java/com/example/whatnownews/di/appModule.kt
@@ -4,25 +4,25 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import com.example.whatnownews.data.remote.favorites.FavoriteArticlesDS
+import com.example.whatnownews.data.repository.ArticleRepositoryImpl
 import com.example.whatnownews.data.repository.AuthRepositoryImpl
+import com.example.whatnownews.domain.repository.ArticleRepository
 import com.example.whatnownews.domain.repository.AuthRepository
 import com.example.whatnownews.domain.usecase.auth.CheckEmailVerifiedUseCase
 import com.example.whatnownews.domain.usecase.auth.ForgotPasswordUseCase
 import com.example.whatnownews.domain.usecase.auth.LoginUseCase
 import com.example.whatnownews.domain.usecase.auth.SendEmailVerificationUseCase
-import com.example.whatnownews.domain.usecase.auth.SignUpUseCase
 import com.example.whatnownews.domain.usecase.auth.SignOutUseCase
+import com.example.whatnownews.domain.usecase.auth.SignUpUseCase
 import com.example.whatnownews.presentation.auth.CheckAuthStatusUseCase
 import com.example.whatnownews.presentation.auth.EmailVerificationViewModel
 import com.example.whatnownews.presentation.auth.ForgotPasswordViewModel
 import com.example.whatnownews.presentation.auth.LoginViewModel
 import com.example.whatnownews.presentation.auth.SignUpViewModel
 import com.example.whatnownews.presentation.auth.SplashViewModel
-import com.example.whatnownews.data.remote.favorites.FavoriteArticlesDS
-import com.example.whatnownews.data.repository.ArticleRepositoryImpl
-import com.example.whatnownews.domain.repository.ArticleRepository
 import com.example.whatnownews.presentation.favorites.FavoritesViewModel
-import org.koin.core.module.dsl.viewModel
+import com.example.whatnownews.presentation.home.HomeViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.GsonBuilder
@@ -30,7 +30,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.factoryOf
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -38,7 +38,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 private const val BASE_URL = "https://newsapi.org/v2/"
-private const val DATABASE_NAME = "what_now_news_db"
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 val appModule = module {
@@ -64,7 +63,6 @@ val appModule = module {
             .addConverterFactory(GsonConverterFactory.create(get()))
             .build()
     }
-    // Retrofit Service
 
     // Firebase
     single { FirebaseAuth.getInstance() }
@@ -75,33 +73,28 @@ val appModule = module {
     single { androidContext().dataStore }
 
     // Data Source
-    single { FavoriteArticlesDS(get(), get()) }
+    singleOf(::FavoriteArticlesDS)
 
 
     // Repositories
-    single { AuthRepositoryImpl(get()) } bind AuthRepository::class
-    //singleOf(::AuthRepositoryImpl) { bind<AuthRepository>() }
+    singleOf(::AuthRepositoryImpl).bind<AuthRepository>()
+    singleOf(::ArticleRepositoryImpl).bind<ArticleRepository>()
 
     // UseCases
     factoryOf(::SignUpUseCase)
     factoryOf(::SendEmailVerificationUseCase)
     factoryOf(::CheckEmailVerifiedUseCase)
     factoryOf(::SignOutUseCase)
-    factory { LoginUseCase(get()) }
-    factory { ForgotPasswordUseCase(repository = get()) }
-    factory { CheckAuthStatusUseCase(repository = get()) }
-
-
+    factoryOf(::LoginUseCase)
+    factoryOf(::ForgotPasswordUseCase)
+    factoryOf(::CheckAuthStatusUseCase)
 
     // ViewModels
     viewModelOf(::SignUpViewModel)
     viewModelOf(::EmailVerificationViewModel)
-    viewModel { LoginViewModel(get(), get()) }
-    viewModel { ForgotPasswordViewModel(forgotPasswordUseCase = get()) }
-    viewModel { SplashViewModel(checkAuthStatusUseCase = get()) }
-    single<ArticleRepository> { ArticleRepositoryImpl(get()) }
-
-    // ViewModels
-    viewModel { FavoritesViewModel(get()) }
-
+    viewModelOf(::LoginViewModel)
+    viewModelOf(::ForgotPasswordViewModel)
+    viewModelOf(::SplashViewModel)
+    viewModelOf(::FavoritesViewModel)
+    viewModelOf(::HomeViewModel)
 }
---
+++ app/src/main/java/com/example/whatnownews/presentation/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.whatnownews.presentation.home
+
+import androidx.lifecycle.ViewModel
+import com.example.whatnownews.domain.repository.AuthRepository
+
+class HomeViewModel(
+    private val authRepository: AuthRepository
+) : ViewModel() {
+    fun logout() {
+        authRepository.signOut()
+    }
+}
